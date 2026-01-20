20eb2c0ab5ad7e488a629e7811fe7fce470f6536
Ziheng <zlia0050@student.monash.edu>
2026-01-20 21:08:09 +1100

    roboflow data download

--- qwen-vl-finetune/captions/download_data.py
+++ qwen-vl-finetune/captions/download_data.py
@@ -1,6 +1,6 @@
 from roboflow import Roboflow
-rf = Roboflow(api_key="BhgQZacev5LVkLmOUayV")
-project = rf.workspace("ailecs-nmbrc").project("40k-dataset-pvktf")
+rf = Roboflow(api_key="")
+project = rf.workspace("").project("40k-dataset-pvktf")
 version = project.version(2)
 dataset = version.download("coco")
                 
